e91ea0f888d4df29c3752edc5749e40908e3dfc5
mymy7337 <minyeong7337@gmail.com>
2025-09-03 17:57:40 +0900

    [Fix] 특수몹 소환패턴 소환위치 수정

--- Team15/Assets/Scriptables/MonsterPattern/Special/MobSpawnPatternSO.cs
+++ Team15/Assets/Scriptables/MonsterPattern/Special/MobSpawnPatternSO.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 [CreateAssetMenu(fileName = "MobSpawn", menuName = "Pattern/MobSpawn")]
 public class MobSpawnPatternSO : PatternDataSO
@@ -7,14 +8,25 @@ public class MobSpawnPatternSO : PatternDataSO
     public int spawnCount;
     public float spawnTime;
     public GameObject[] spawnPool;
+    public Vector2[] spawnPositions;
     private GameObject[] monsters = new GameObject[2];
+    private Vector2[] positions = new Vector2[2];
+    
 
     public override IEnumerator Execute(Monster monster)
     {
+        for(int i = 0; i < positions.Length; i++)
+        {
+            positions[i] = spawnPositions[Random.Range(0, spawnPositions.Length)];
+            while(i != 0 &&spawnPositions[i] == spawnPositions[i - 1])
+            {
+                positions[i] = spawnPositions[Random.Range(0, spawnPositions.Length)];
+            }
+        }
         monster.stateMachine.ChangeState(monster.stateMachine.SturnState);
         for (int i = 0; i < monsters.Length; i++)
         {
-            monsters[i] = Instantiate(spawnPool[Random.Range(0, spawnPool.Length)], monster.transform.position + new Vector3(i, 0, 0), Quaternion.identity);
+            monsters[i] = Instantiate(spawnPool[Random.Range(0, spawnPool.Length)], positions[i], Quaternion.identity);
         }
         monster.canSpawn = false;
         yield return new WaitForSeconds(spawnTime);
