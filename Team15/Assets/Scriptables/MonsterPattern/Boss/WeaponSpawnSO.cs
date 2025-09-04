5b07ad2a126c73991e00b066b8111a6ebba1f1a4
mymy7337 <minyeong7337@gmail.com>
2025-09-04 11:44:09 +0900

    [Feat] 곤뇽 무기소환패턴 추가
    (임시) 무기소환기능만 추가
    데미지 기능은 미구현

--- Team15/Assets/Scriptables/MonsterPattern/Boss/WeaponSpawnSO.cs
+++ Team15/Assets/Scriptables/MonsterPattern/Boss/WeaponSpawnSO.cs
@@ -4,10 +4,20 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "WeaponSpawn", menuName = "Pattern/WeaponSpawn")]
 public class WeaponSpawnSO : PatternDataSO
 {
+    public int spawnNum;
     public GameObject[] weaponPrefabs;
 
     public override IEnumerator Execute(Monster monster)
     {
-        throw new System.NotImplementedException();
+        monster.stateMachine.ChangeState(monster.stateMachine.SturnState);
+        for (int i = 0; i < spawnNum; i++)
+        {
+            var go = Instantiate(weaponPrefabs[Random.Range(0, weaponPrefabs.Length)], monster.transform.position, Quaternion.identity);
+            var rg = go.GetComponent<Rigidbody2D>();
+            rg.AddForce(new Vector2(1, Random.Range(-3, 3)) * Random.Range(5, 15), ForceMode2D.Impulse);
+        }
+        yield return new WaitForSeconds(2f);
+        monster.canSpawn = false;
+        monster.StartCoroutine(monster.CheckInPattern());
     }
 }
