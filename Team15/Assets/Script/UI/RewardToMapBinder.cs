9a701cb4d57521769d78509e06d4227fae11af79
SJW1737 <sic470212@naver.com>
2025-09-05 16:34:37 +0900

    [feat]각 ui, 오브젝트 프리팹화

---
+++ Team15/Assets/Script/UI/RewardToMapBinder.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RewardToMapBinder : MonoBehaviour
+{
+    [SerializeField] RewardUI rewardUI;
+    [SerializeField] MapSelectUI mapUI;
+    [SerializeField] int floor = 1;
+
+    void OnEnable() { if (rewardUI) rewardUI.OnFinished += OpenMap; }
+    void OnDisable() { if (rewardUI) rewardUI.OnFinished -= OpenMap; }
+
+    void OpenMap() => mapUI.Open(floor);
+}
